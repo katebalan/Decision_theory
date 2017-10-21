1c1854f5e568f120a6679098a8f65847cc379271
Kate Balan <ket11141@gmail.com>
2017-10-22 00:27:51 +0300

    add FFA, but not work correctly

--- lab_tpr3/something2.py
+++ lab_tpr3/something2.py
@@ -5,17 +5,17 @@ from ContainerClass import Container
 
 document = open('1.txt', 'r')
 
+container_capacity = 100
 boxes = []
 for number in document.readline().split():
     boxes.append(int(number))
 
 print "DATA: {}".format(boxes)
 
+# Next Fit Algorithm
 containers = []
 containers.append(Container())
-container_capacity = 100
 
-# Next Fit Algorithm
 for box in boxes:
     if containers[len(containers) - 1].getContainerWeight() + box <= container_capacity:
         containers[len(containers) - 1].add(box)
@@ -25,5 +25,28 @@ for box in boxes:
 
 print "********"
 print "Next Fit Algorithm:"
+for container in containers:
+    container.printContainer()
+
+# First Fit Algorithm
+containers = []
+containers.append(Container())
+
+for box in boxes:
+    if containers[len(containers) - 1].getContainerWeight() + box <= container_capacity:
+        containers[len(containers) - 1].add(box)
+    else:
+        placed = False
+        for i in range(len(containers) - 1):
+            if containers[len(containers) - 1].getContainerWeight() + box <= container_capacity:
+                containers[i].add(box)
+                placed = True
+                break
+        if not placed:
+            containers.append(Container())
+            containers[len(containers) - 1].add(box)
+
+print "********"
+print "First Fit Algorithm:"
 for container in containers:
     container.printContainer()
