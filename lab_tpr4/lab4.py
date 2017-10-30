aa9772870b0671631c19414243fe632f5ffdf3ae
Kate Balan <kate.balan@inuits.eu>
2017-10-30 13:48:53 +0200

    add relative majority

--- lab_tpr4/lab4.py
+++ lab_tpr4/lab4.py
@@ -1,11 +1,30 @@
+import operator
 
 train_document = open('1.txt', 'r')
 
 profile = []
 for line in train_document:
     profile.append([])
-    for symb in line.split():
-        profile[len(profile) - 1].append(symb)
+    for symbol in line.split():
+        profile[len(profile) - 1].append(symbol)
 
 train_document.close()
 print profile
+
+def relative_majority(profile):
+    result = {}
+    for i in range(len(profile[0])):
+        if profile[1][i] not in result:
+            result[profile[1][i]] = int(profile[0][i])
+        else:
+            temp = result[profile[1][i]]
+            result[profile[1][i]] = int(profile[0][i]) + temp
+
+    winner = max(result.iteritems(), key = operator.itemgetter(1))[0]
+    return winner
+
+def condorcet(profile):
+    pass
+
+
+print "Winner (relative majority): {}".format(relative_majority(profile))
