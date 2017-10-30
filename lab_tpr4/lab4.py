20d21a3c9ea5a97ada80810fa3af5797f7576234
Kate Balan <kate.balan@inuits.eu>
2017-10-30 15:27:54 +0200

    Condorcet

--- lab_tpr4/lab4.py
+++ lab_tpr4/lab4.py
@@ -1,30 +1,55 @@
-import operator
+# import operator
+import itertools
+from collections import defaultdict
 
-train_document = open('1.txt', 'r')
+document1 = open('1.txt', 'r')
 
-profile = []
-for line in train_document:
-    profile.append([])
+profiles = []
+print "Profiles: "
+
+for line in document1:
+    profiles.append([])
     for symbol in line.split():
-        profile[len(profile) - 1].append(symbol)
+        profiles[len(profiles) - 1].append(symbol)
+    print profiles[len(profiles) - 1]
 
-train_document.close()
-print profile
+document1.close()
 
-def relative_majority(profile):
+document2 = open('2.txt', 'r')
+
+profiles_weight = []
+for number in document2.readline().split():
+    profiles_weight.append(int(number))
+
+document2.close()
+
+print "Profiles weight: {}".format(profiles_weight)
+
+def relative_majority(profiles, profile_weight):
     result = {}
-    for i in range(len(profile[0])):
-        if profile[1][i] not in result:
-            result[profile[1][i]] = int(profile[0][i])
+    for i in range(len(profiles[0])):
+        if profiles[0][i] not in result:
+            result[profiles[0][i]] = int(profile_weight[i])
         else:
-            temp = result[profile[1][i]]
-            result[profile[1][i]] = int(profile[0][i]) + temp
-
-    winner = max(result.iteritems(), key = operator.itemgetter(1))[0]
+            temp = result[profiles[0][i]]
+            result[profiles[0][i]] = int(profile_weight[i]) + temp
+    winner = max(result, key=result.get)
     return winner
 
-def condorcet(profile):
-    pass
+def condorcet(profiles, profiles_weight):
+    profiles_tranc = zip(* profiles)
+    results = defaultdict(int)
+    for iter in range(len(profiles_weight)):
+        for c1, c2 in itertools.combinations(profiles_tranc[iter], 2): # combinations('ABCD', 2) --> AB AC AD BC BD CD
+            assert (c1 != c2)
+            mini, maxi, res = (c1, c2, profiles_weight[iter]) if c1 < c2 else (c2, c1, -profiles_weight[iter])
+            results[mini, maxi] += res
+    for c in {c for profile in profiles_tranc for c in profile}:
+        # TO DO understand if statement
+        if (all((res > 0 if c == mini else res < 0) for (mini, maxi), res in results.items() if c in [mini, maxi])):
+            return c
+    return None
 
 
-print "Winner (relative majority): {}".format(relative_majority(profile))
+print "Winner (relative majority): {}".format(relative_majority(profiles, profiles_weight))
+print "Winner (Condorcet): {}".format(condorcet(profiles, profiles_weight))
